7ec40b25fd017e14d9e9334aeca0f6f866e6b467
Agustin <delgiudiceagustin@outlook.com.ar>
2024-07-24 14:15:31 -0300

    Configuraciones necesarias para Emails

--- NetCoreWebsitesBL/Configurations/EmailSettings.cs
+++ NetCoreWebsitesBL/Configurations/EmailSettings.cs
@@ -2,7 +2,7 @@ namespace NetCoreWebsitesBL.Configurations
 {
     public class EmailSettings
     {
-        public string FromEmail { get; set; }
-        public string FromName { get; set; }
+        public string FromEmail { get; set; } = string.Empty;
+        public string FromName { get; set; } = string.Empty;
     }
 }
